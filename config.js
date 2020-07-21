452402d2c262f01c40a269f8b1160795dc35b7de
Kamile Inciuraite <kamile.inciuraite@stud.vgtu.lt>
2020-07-21 12:59:12 +0300

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '567833007232883',
+        clientSecret: '70b7f8b23c267b3b67c4de314791de64'
+    }
 }
