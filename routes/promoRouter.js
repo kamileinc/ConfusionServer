43a89b29cfe754573986b9c1ac137164fba625e4
Kamile Inciuraite <kamile.inciuraite@stud.vgtu.lt>
2020-07-21 15:53:10 +0300

    Integrating Client and Server

--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -12,8 +12,8 @@ promotionRouter.use(bodyParser.json());
 
 promotionRouter.route('/')
 .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
-    .get(cors.cors, (req, res, next) => {
-        Promotions.find({})
+.get(cors.cors, (req,res,next) => {
+    Promotions.find(req.query)
             .then((promotions) => {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
